c1ab11ee88bce1f57feb141826719f878889aa4c
CesareMazzilli <cesaremazzilli98@gmail.com>
2024-12-16 13:30:15 +0100

    feat: done

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,7 +1,9 @@
 import style from "./AppMain.module.css";
 import PhotoCard from "./PhotoCard";
+import posts from "./posts";
 
 function AppMain() {
+    const postHTML = posts.map((curElem) => curElem.published && <PhotoCard title={curElem.title} image={curElem.image} content={curElem.content} tags={curElem.tags}/>);
     return (
         <main>
             <div className={style.container}>
---
+++ src/components/posts.js
@@ -0,0 +1,38 @@
+const posts = [
+    {
+      id: 1,
+      title: "Pizza",
+      image: './images/pizza.jpg',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["html", "css"],
+      published: true,
+    },
+    {
+      id: 2,
+      title: "Pane",
+      image: './images/pane.jpg',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["js", "css"],
+      published: true,
+    },
+    {
+      id: 3,
+      title: "Pasta",
+      image: './images/pasta.jpg',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["js", "php"],
+      published: true,
+    },
+    {
+      id: 4,
+      title: "Pomodoro",
+      image: './images/pomodoro.jpg',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["html"],
+      published: false,
+    },
+  ];
